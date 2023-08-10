76e8481cd685e9be1aec864acd33bf153688595e
li-guohao <git@liguohao.cn>
2023-08-10 20:39:23 +0800

    try add subtitle url.

--- uranus-android/app/src/main/java/run/ikaros/uranus/ExoPlayerActivity.java
+++ uranus-android/app/src/main/java/run/ikaros/uranus/ExoPlayerActivity.java
@@ -2,16 +2,20 @@ package run.ikaros.uranus;
 
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.StyledPlayerView;
 import com.google.android.exoplayer2.ui.SubtitleView;
+import com.google.android.exoplayer2.util.MimeTypes;
+import com.google.common.collect.ImmutableList;
 
 import java.util.Objects;
 
@@ -27,7 +31,20 @@ public class ExoPlayerActivity extends AppCompatActivity {
         ExoPlayerView playerView = findViewById(R.id.exoPlayerVideoView);
         exoPlayer = new ExoPlayer.Builder(ExoPlayerActivity.this).build();
         playerView.setPlayer(exoPlayer);
+
         MediaItem mediaItem = MediaItem.fromUri(VideoTmp.EMBED_ASS_H265_URL);
+
+        // MediaItem.SubtitleConfiguration subtitle =
+        //         new MediaItem.SubtitleConfiguration.Builder(Uri.parse(VideoTmp.H265_CHS_ASS_URL))
+        //                 .setMimeType(MimeTypes.APPLICATION_SUBRIP) // The correct MIME type (required).
+        //                 .setLanguage("CHS") // The subtitle language (optional).
+        //                 // .setSelectionFlags() // Selection flags for the track (optional).
+        //                 .build();
+        // MediaItem mediaItem  = new MediaItem.Builder()
+        //         .setUri(VideoTmp.H265_URL)
+        //         .setSubtitleConfigurations(ImmutableList.of(subtitle))
+        //         .build();
+
         exoPlayer.setMediaItem(mediaItem);
         exoPlayer.prepare();
         playerView.setControllerHideOnTouch(false);
